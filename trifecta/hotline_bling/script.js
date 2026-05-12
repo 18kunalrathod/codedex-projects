32098454d5a4ec5c05f55a264ecf3f4e44dd67ee
Kunal Rathod <kunalrathod@Kunals-MacBook-Air.local>
2026-05-12 21:45:44 +0530

    updated

---
+++ trifecta/hotline_bling/script.js
@@ -0,0 +1,6 @@
+const drakePicTwo = document.querySelector("#drake-pic-2")
+const heading = document.querySelector("#heading-2")
+
+drakePicTwo.src ="https://i.imgur.com/RGbh6zY.png"
+
+heading.innerText = "Learn By Doing"
